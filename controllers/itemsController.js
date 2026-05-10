6be26d30bee816a69ba865bca4f245e71ad9eef7
arturtugui <146761295+arturtugui@users.noreply.github.com>
2026-05-10 21:28:56 +0300

    fix: solve wrong import from store

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,49 +1,49 @@
-import store from '../data/store.js'
+import {loadItems, saveItems} from '../data/store.js'
 
 export async function getAllItems() {
-  return await store.loadItems()
+  return await loadItems()
 }
 
 export async function getItemById(id) {
-  const items = await store.loadItems()
-  return items.find(item => item.id === id)
+  const items = await loadItems()
+  return items.find(item => item.id === id) // returns 1st mathching item or undefined if not found
 }
 
 export async function addItem(item) {
-  const items = await store.loadItems()
+  const items = await loadItems()
   items.push(item)
-  await store.saveItems(items)
+  await saveItems(items)
 }
 
 export async function updateItem(updatedItem) {
-  const items = await store.loadItems()
-  const index = items.findIndex(item => item.id === updatedItem.id)
+  const items = await loadItems()
+  const index = items.findIndex(item => item.id === updatedItem.id) // returns index of 1st matching item or -1 if not found
   if (index !== -1) {
     items[index] = updatedItem
-    await store.saveItems(items)
+    await saveItems(items)
   }
 }
 
 export async function deleteItem(id) {
-  const items = await store.loadItems()
-  const filteredItems = items.filter(item => item.id !== id)
-  await store.saveItems(filteredItems)
+  const items = await loadItems()
+  const filteredItems = items.filter(item => item.id !== id) // returns new array with all matches
+  await saveItems(filteredItems)
 }
 
 export async function hideItem(id) {
-  const items = await store.loadItems()
+  const items = await loadItems()
   const index = items.findIndex(item => item.id === id)
     if (index !== -1) {
     items[index].isHidden = true
-    await store.saveItems(items)
+    await saveItems(items)
     }
 }
 
 export async function unhideItem(id) {
-  const items = await store.loadItems()
+  const items = await loadItems()
   const index = items.findIndex(item => item.id === id)
     if (index !== -1) {
         items[index].isHidden = false
-        await store.saveItems(items)
+        await saveItems(items)
     }
 }
