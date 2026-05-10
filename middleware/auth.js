ffc50c4982cb099a0fa4b59fa3130a77048e9ef1
arturtugui <146761295+arturtugui@users.noreply.github.com>
2026-05-10 22:41:38 +0300

    feat: add middleware for auth to check if the user is authenticated

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken'
+
+// 401 - Unauthorized
+
+export function authenticate(req, res, next) {
+    const header = req.headers['authorization']
+    if (!header) {
+        return res.status(401).json({ error: 'Authorization header missing' })
+    }
+
+    const token = header.split(' ')[1] // Expecting "Bearer <token>"
+    if (!token) {
+        return res.status(401).json({ error: 'Token missing' })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) // check if token is valid and not expired
+        req.user = decoded // attach user info to request object (as another field)
+        next() // pass control to the next middleware or route handler
+    } catch (error) {
+        return res.status(401).json({ error: 'Invalid token' })
+    }
+}
