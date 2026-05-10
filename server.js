eed2b5984ad25e61166e246f83d74328a01a4ce9
arturtugui <146761295+arturtugui@users.noreply.github.com>
2026-05-10 17:33:25 +0300

    docs: add comments to the file

--- server.js
+++ server.js
@@ -6,14 +6,11 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
+//middleware - runs between request arriving and route handlers
 app.use(express.json()) // parse JSON request bodies
 
 app.get('/', (req, res) => {
   res.json({ message: 'PIT API Server running' })
-})
-
-app.get('/items', (req, res) => {
-  res.json({ items: [] }) // send JSON response
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
